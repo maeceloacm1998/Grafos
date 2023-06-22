82522700e28d91e33fa82c48c51fd8c2b3972fdd
Igor Atilar <igoratilar10@gmail.com>
2023-06-22 08:43:18 -0300

    docs: add adjacency list java doc

--- src/libs/AdjacencyList.java
+++ src/libs/AdjacencyList.java
@@ -14,18 +14,35 @@ import models.Edge;
 import utils.Graph;
 
 public class AdjacencyList implements IAdjacencyList {
+    private final String WITHOUT_WEIGHT = "0";
 
     Map<String, List<Edge>> adjacentList;
 
+    /**
+     * Construtor da classe AdjacencyList
+     *
+     * @param list Lista de itens do grafo (vértices e arestas)
+     */
     public AdjacencyList(List<GraphItem> list) {
         this.adjacentList = new HashMap<>();
         init(list);
     }
 
+
+    /**
+     * Inicializa a lista adjascente
+     *
+     * @param list Lista de itens do grafo (vértices e arestas)
+     */
     private void init(List<GraphItem> list) {
         handleAdjacencyList(list);
     }
 
+    /**
+     * Cria a lista adjascente
+     *
+     * @param list Lista de itens do grafo (vértices e arestas)
+     */
     private void handleAdjacencyList(List<GraphItem> list) {
         list.forEach(GraphItemModel -> {
             String vertex = GraphItemModel.getVertex();
@@ -39,10 +56,22 @@ public class AdjacencyList implements IAdjacencyList {
         });
     }
 
+    /**
+     * Retorna a lista adjascente
+     * 
+     * @return Lista adjascente
+     */
     public Map<String, List<Edge>> getAdjacentList() {
         return adjacentList;
     }
 
+
+    /**
+     * Insere um vértice na lista adjascente
+     * 
+     * @param vertex Vértice a ser inserido
+     * @param edge Aresta a ser inserida
+     */
     @Override
     public void insert(String vertex, String edge) {
         String connection = Graph.getConnection(edge);
@@ -53,6 +82,12 @@ public class AdjacencyList implements IAdjacencyList {
         this.adjacentList.put(vertex, edges);
     }
 
+    /**
+     * Remove um vértice da lista adjascente
+     * 
+     * @param vertex Vértice a ser removido
+     * @param edge Aresta a ser removida
+     */
     @Override
     public void remove(String vertex, String edge) {
         String connection = Graph.getConnection(edge);
@@ -61,36 +96,70 @@ public class AdjacencyList implements IAdjacencyList {
         this.adjacentList.put(vertex, edges);
     }
 
+    /**
+     * Verifica se o vertice é ponderado
+     * 
+     * @param vertex Vértice a ser verificado
+     */
     @Override
     public Boolean isVertexThoughtful(String vertex) {
         List<Edge> edges = this.adjacentList.getOrDefault(vertex, new ArrayList<>());
-        return edges.stream().anyMatch(e -> !e.getWeight().equals("0"));
+        return edges.stream().anyMatch(e -> !e.getWeight().equals(WITHOUT_WEIGHT));
     }
 
+
+
+    /**
+     * Verifica se o vertice é rotulado
+     * 
+     * @param vertex Vértice a ser verificado
+     */
     @Override
     public Boolean isVertexLabeled(String vertex) {
         List<Edge> edges = this.adjacentList.getOrDefault(vertex, new ArrayList<>());
         return edges.stream().anyMatch(e -> !Graph.isWeightNumeric(e.getWeight()));
     }
 
+    /**
+     * Verifica se a aresta é ponderada
+     * 
+     * @param edge Aresta a ser verificada
+     */
     @Override
     public Boolean isEdgeThoughtful(String edge) {
         String weight = Graph.getWeight(edge);
-        return !weight.equals("0") && Graph.isWeightNumeric(weight);
+        return !weight.equals(WITHOUT_WEIGHT) && Graph.isWeightNumeric(weight);
     }
 
+    /**
+     * Verifica se a aresta é rotulada
+     * 
+     * @param edge Aresta a ser verificada
+     */
     @Override
     public Boolean isEdgeLabeled(String edge) {
         String weight = Graph.getWeight(edge);
         return !Graph.isWeightNumeric(weight);
     }
 
+    /**
+     * Verifica se o vértice é adjascente ao outro vértice
+     * 
+     * @param vertex Vértice a ser verificado
+     * @param adjacentVertex Vértice adjascente
+     */
     @Override
     public Boolean isAdjacentVertex(String vertex, String adjacentVertex) {
         List<Edge> edges = this.adjacentList.getOrDefault(vertex, new ArrayList<>());
         return edges.stream().anyMatch(e -> e.getConnection().equals(adjacentVertex));
     }
 
+    /**
+     * Verifica se a aresta é adjascente a um vértice
+     * 
+     * @param vertex Vértice a ser verificado
+     * @param adjacentEdge Aresta adjascente
+     */
     @Override
     public Boolean isAdjacentEdge(String vertex, String adjacentEdge) {
         List<Edge> edges = this.adjacentList.getOrDefault(vertex, new ArrayList<>());
@@ -101,28 +170,53 @@ public class AdjacencyList implements IAdjacencyList {
         });
     }
 
+    /**
+     * Verifica se existe arestas na lista adjascente
+     * 
+     * @return Boolean se existir arestas na lista adjascente
+     */
     @Override
     public Boolean existEdge() {
         return this.adjacentList.values().stream().anyMatch(e -> !e.isEmpty());
     }
 
 
+    /**
+     * Retorna a quantidade de arestas na lista adjascente
+     * 
+     * @return Quantidade de arestas na lista adjascente
+     */
     @Override
     public Integer quantityEdge() {
         return this.adjacentList.values().stream().mapToInt(List::size).sum();
     }
 
 
+    /**
+     * Retorna a quantidade de vértices na lista adjascente
+     * 
+     * @return Quantidade de vértices na lista adjascente
+     */
     @Override
     public Integer quantityVertex() {
         return this.adjacentList.size();
     }
 
+    /**
+     * Retorna se a lista adjascente está vazia
+     * 
+     * @return Boolean se a lista adjascente está vazia
+     */
     @Override
     public Boolean isEmpty() {
         return this.adjacentList.isEmpty();
     }
 
+    /**
+     * Retorna se a lista adjascente é completa
+     * 
+     * @return Boolean se a lista adjascente é completa
+     */
     @Override
     public Boolean isComplet() {
         List<String> vertices = new ArrayList<>(this.adjacentList.keySet());
@@ -136,23 +230,11 @@ public class AdjacencyList implements IAdjacencyList {
         return true;
     }
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        for (Map.Entry<String, List<Edge>> entry : this.adjacentList.entrySet()) {
-            String vertex = entry.getKey();
-            List<Edge> edges = entry.getValue();
-            builder.append(vertex).append(": ");
-            for (Edge edge : edges) {
-                String connection = edge.getConnection();
-                String weight = edge.getWeight();
-                builder.append("(").append(connection).append(", ").append(weight).append(") ");
-            }
-            builder.append("\n");
-        }
-        return builder.toString();
-    }
-
+    /**
+     * Exporta a lista adjascente para CSV em formato suportado pelo Gephi
+     * 
+     * @param filePath Caminho do arquivo CSV
+     */
     @Override
     public void exportToCSV(String filePath) {
         try (PrintWriter writer = new PrintWriter(new File(filePath))) {
@@ -171,6 +253,11 @@ public class AdjacencyList implements IAdjacencyList {
         }
     }
 
+    /**
+     * Importa a lista adjascente de um arquivo CSV em formato suportado pelo Gephi
+     * 
+     * @param filePath Caminho do arquivo CSV
+     */
     @Override
     public void importFromCSV(String filePath) {
         try (Scanner scanner = new Scanner(new File(filePath))) {
@@ -181,7 +268,7 @@ public class AdjacencyList implements IAdjacencyList {
                 String vertex = tokens[0];
                 List<Edge> edges = new ArrayList<>();
                 for (int i = 1; i < tokens.length; i++) {
-                    edges.add(new Edge(tokens[i], "0"));
+                    edges.add(new Edge(tokens[i], WITHOUT_WEIGHT));
                 }
                 adjacentList.put(vertex, edges);
             }
@@ -190,5 +277,24 @@ public class AdjacencyList implements IAdjacencyList {
             System.err.println("Erro ao exportar lista adjascente para CSV: " + e.getMessage());
         }
     }
+
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<String, List<Edge>> entry : this.adjacentList.entrySet()) {
+            String vertex = entry.getKey();
+            List<Edge> edges = entry.getValue();
+            builder.append(vertex).append(": ");
+            for (Edge edge : edges) {
+                String connection = edge.getConnection();
+                String weight = edge.getWeight();
+                builder.append("(").append(connection).append(", ").append(weight).append(") ");
+            }
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
+
 
 }
